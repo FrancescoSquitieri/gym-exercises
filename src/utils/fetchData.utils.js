2c2388429360de30ed84c318e5942c8bdb853a08
FrancescoSquitieri <101020234+FrancescoSquitieri@users.noreply.github.com>
2022-08-11 21:22:03 +0200

    added private key on env

--- src/utils/fetchData.utils.js
+++ src/utils/fetchData.utils.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '0ed4717a49msh2005c4c67d1c9e0p1b4100jsn3dd7d3a5b0bb',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
     'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
   }
 };
@@ -9,7 +9,7 @@ export const exerciseOptions = {
 export const exercisesYoutubeVideosOptions = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': '0ed4717a49msh2005c4c67d1c9e0p1b4100jsn3dd7d3a5b0bb',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
