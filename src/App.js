69979bdb0348b71b163c5c78d2973c17d4f733cb
FrancescoSquitieri <101020234+FrancescoSquitieri@users.noreply.github.com>
2022-08-12 00:30:35 +0200

    added modal notice for rapidapi

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ import Navbar from './components/Navbar.component';
 import Home from './pages/Home.pages';
 import ExerciseDetail from './pages/ExerciseDetail.pages';
 import Footer from './components/Footer.component';
+import Modal from './components/Modal.component';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [isOpenModal, setIsOpenModal] = useState(null);
+
+  useEffect(() => {
+    const openModal = JSON.parse(localStorage.getItem('isOpenModal'));
+    console.log(openModal)
+    if (openModal !== false) {
+      console.log("entro qui")
+      localStorage.setItem('isOpenModal', true);
+      setIsOpenModal(JSON.parse(localStorage.getItem('isOpenModal')))
+    } else {
+      setIsOpenModal(JSON.parse(localStorage.getItem('isOpenModal')))
+    }
+  }, [])
+
   return (
     <Box
       width='400px'
@@ -17,6 +33,7 @@ function App() {
       }}
       m='auto'
     >
+      <Modal open={isOpenModal} setOpen={setIsOpenModal} />
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
---
+++ src/components/Modal.component.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+
+const Modal = ({ open, setOpen }) => {
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleCloseInLocalStorage = () => {
+    localStorage.setItem('isOpenModal', false);
+    setOpen(false);
+  }
+
+  return (
+    <div>
+      <Dialog
+        fullScreen={fullScreen}
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          <Typography fontSize='30px' textTransform='uppercase'>
+            Important Notice
+          </Typography>
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            The data used are taken from "rapidapi", a very useful third party service that exposes many data but which unfortunately, after 500 calls to an endpoint in a single month you have to wait for the next month to be able to make a further 500 calls to the maximum (free floor , I am poor but very determined! :)), if something should go wrong, I am very sorry but the maximum calls for the endpoints with which I developed this app have been reached
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseInLocalStorage}>
+            Close and remember in localstorage
+          </Button>
+          <Button onClick={handleClose} color='success'>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  )
+}
+
+export default Modal
